251c879fc5fec953a2694dbbb88575b826d6af2f
Ilya <kanaplyasm@gmail.com>
2021-10-15 12:26:43 +0300

    add second part

--- src/noe.py
+++ src/noe.py
@@ -3,3 +3,11 @@ for a in range(11):
         b = b * a
         print(b, end = '\t')
     print()
+
+print()
+
+for a in range(11):
+    for b in range(a, 11):
+        b = b * a
+        print(b, end='\t')
+    print()
