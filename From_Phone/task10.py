d01850b825b362f556147d1629b2e73461418eeb
Ilya <kanaplyasm@gmail.com>
2021-10-26 15:13:37 +0300

    Готово 10-е

---
+++ From_Phone/task10.py
@@ -0,0 +1,14 @@
+number = int(input('Сколько чисел: '))
+list_1 = []
+count = 0
+for _ in range(number):
+    list_1.append(int(input('Введите число: ')))
+if list_1 != list_1[::-1]:
+    while list_1 != list_1[::-1]:
+        list_1.insert(number, list_1[count])
+        count += 1
+    print(count)
+    print(list_1[number::])
+else:
+    print('Последовательность Симметрична')
+
