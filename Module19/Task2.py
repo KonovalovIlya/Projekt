c01014a73aab7bb1808fc8165924b048ef43caa2
Ilya <kanaplyasm@gmail.com>
2021-11-05 18:38:58 +0300

    М19-2

--- Module19/Task2.py
+++ Module19/Task2.py
@@ -1,25 +1,22 @@
 country_all = int(input('Кол-во стран: '))
 country_dict = {}
+
 for country_number in range(1, country_all + 1):
     country_info = input('{} страна: '.format(country_number))
     country_info = country_info.split()
     country_dict[country_number] = {country_info[0]: country_info[1:]}
-    # 2 страна: Германия Берлин Лейпциг Мюнхен
-print(country_dict)
+
+city_number = 0
+
 while True:
-    city_number = 1
+    city_number += 1
+    true_chek = False
     city = input('{} город: '.format(city_number))
-    for n in range(1, len(country_dict) + 1):
+    for n in country_dict:
         for country in country_dict[n]:
             if city in country_dict[n][country]:
+                true_chek = True
                 print('Город {0} расположен в стране {1}'.format(city, country))
-    else:
+    if not true_chek:
         print('По городу {} данных нет.'.format(city))
         break
-    city_number = 1
-#
-# 2 город: Мюнхен
-# Город Мюнхен расположен в стране Германия.
-#
-# 3 город: Рим
-# По городу Рим данных нет.
