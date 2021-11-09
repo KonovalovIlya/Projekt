00f5c85e93ba85cdd1261dbd5094deffb00d999d
Ilya <kanaplyasm@gmail.com>
2021-11-09 15:05:57 +0300

    done Task 19-7

---
+++ Module19/Task19-7.py
@@ -0,0 +1,14 @@
+orders = int(input('Введите кол-во заказво: '))
+order_dict = {}
+for number in range(1, orders + 1):
+    order = input('{} заказ: '.format(number)).split()
+    if order[0] in order_dict.keys() and not order[1] in order_dict[order[0]].keys():
+        order_dict[order[0]][order[1]] = int(order[2])
+    elif order[0] in order_dict.keys() and order[1] in order_dict[order[0]].keys():
+        order_dict[order[0]][order[1]] += int(order[2])
+    else:
+        order_dict[order[0]] = {order[1]: int(order[2])}
+for client in order_dict.keys():
+    print('{}:'.format(client))
+    for pizza in order_dict[client].keys():
+        print('\t{0}: {1}'.format(pizza, order_dict[client][pizza]))
