70d7c72cb5f8574005335eaa7789e5a94e723b31
Ilya <kanaplyasm@gmail.com>
2021-11-10 17:28:58 +0300

    Done Task 19-8

--- Module19/Task19-8.py
+++ Module19/Task19-8.py
@@ -1,32 +1,21 @@
-# Пример реализации:
-# Введите максимальное число: 10
-#
-# Нужное число есть среди вот этих чисел: 1 2 3 4 5
-# Ответ Артёма: Да
-#
-# Нужное число есть среди вот этих чисел: 2 4 6 8 10
-# Ответ Артёма: Нет
-#
-# Нужное число есть среди вот этих чисел: Помогите!
-# Артём мог загадать следующие числа: 1 3 5
-#
-import numbers
 import random
 
 number_max = int(input('Введите максимальное число: '))
 number_all = set(list(range(1, number_max + 1)))
-number_goal = set(random.randint(1, number_max) for _ in range(random.randint(1, number_max // 2)))
-print(number_goal)
+number_goal = random.randint(1, number_max)
 while True:
     number_search = input('Нужное число есть среди вот этих чисел: ')
     if number_search == 'Помогите!':
-        print('Артём мог загадать следующие числа: {}'.format(number_goal))
+        print('Артём мог загадать следующие числа: {}'.format(number_all.intersection(number_search)))
         break
     number_search = {int(n) for n in number_search.split()}
-    j = number_search.intersection(number_goal)
-    if number_goal in number_search.intersection(number_goal):
+    if number_goal in number_search and len(number_search) > 1:
+        number_all = number_all.intersection(number_search)
         print('Ответ Артёма: Да')
+    elif number_goal == number_search:
+        print('Угадал')
+        break
     else:
         print('Ответ Артёма: Нет')
-
+        number_all = number_all.difference(number_search)
 
