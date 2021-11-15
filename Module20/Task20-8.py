7b35a6bcbe3572b5be0d2e19f253d7151a92d69e
Ilya <kanaplyasm@gmail.com>
2021-11-15 18:15:59 +0300

    Done Tasks 20-8

---
+++ Module20/Task20-8.py
@@ -0,0 +1,17 @@
+phonebook = dict()
+while True:
+    if len(phonebook) == 0:
+        print('Здесь пока нет контактов')
+    print('Что нужно делать: «Поиск человека по фамилии» или «Добавить контакт»?')
+    operation = input().lower()
+    if operation == 'поиск человека по фамилии' or operation == 'поиск':
+        surname = input('Введите фамилию контакта: ').upper()
+        for i, j in phonebook.items():
+            if surname in i:
+                print(*i, '-', j)
+            else:
+                print('Ничего не надено')
+    elif operation == 'добавить контакт':
+        contact_id = input('Введите имя и фамилию контакта: ').upper().split()
+        contact_phone_number = int(input('Введите номер телефона: '))
+        phonebook[(contact_id[0], contact_id[1])] = contact_phone_number
