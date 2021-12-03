a75a8cd783214369301a793ed29e0f3ab3b63cbd
Ilya <kanaplyasm@gmail.com>
2021-12-03 16:52:34 +0300

    Not working Tasks 21-6

--- Module21/Task21-6.py
+++ Module21/Task21-6.py
@@ -1,22 +1,24 @@
 def site_consruct(number, structure):
-    brend = input('Введите название продукта для нового сайта: ')
-    site_list = {i: search_function(structure, brend, 'title', 'h2') for i in range(number)}
-    print(site_list)
+    for i in range(number):
+        brend = input('Введите название продукта для нового сайта: ')
+        site_list[i] = search_function(structure, brend, 'title', 'h2')
+        print(site_list)
 
 
 def search_function(structure, brend, *args):
     for arg in args:
         if arg in structure.keys():
-            structure[arg] = structure[arg].replace('{}', brend, 1)
+            if arg == 'title':
+                structure[arg] = structure.get(arg).replace(structure.get(arg)[13:-9], brend, 1)
+            elif arg == 'h2':
+                structure[arg] = structure.get(arg).replace(structure.get(arg)[27:], brend, 1)
         for substructure in structure.values():
             if isinstance(substructure, dict):
-                result = search_function(substructure, brend, *args)
+                search_function(substructure, brend, *args)
     return structure
 
 
-site_amount = int(input('Сколько сайтов: '))
-
-site_list = []
+site_list = {}
 site = {
     'html': {
         'head': {
@@ -30,4 +32,5 @@ site = {
     }
 }
 
+site_amount = int(input('Сколько сайтов: '))
 site_consruct(site_amount, site)
