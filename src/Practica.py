70c0361c4d73616a7e31cfafd9033092b6849080
Ilya <kanaplyasm@gmail.com>
2021-12-20 16:30:22 +0300

    Done Tasks 22-8

--- src/Practica.py
+++ src/Practica.py
@@ -47,28 +47,48 @@
 # answer.write(str(summ))
 # answer.close()
 
-import os
 
-def search(cur_path, list_=[]):
-    for i_elem in os.listdir(cur_path):
-        path = os.path.join(cur_path, i_elem)
+# Работа с файлами, путями.
+# import os
+#
+# def search(cur_path, list_=[]):
+#     for i_elem in os.listdir(cur_path):
+#         path = os.path.join(cur_path, i_elem)
+#
+#         if i_elem.endswith('.py'):
+#             list_.append(path)
+#         if os.path.isdir(path):
+#             result = search(path)
+#     return list_
+#
+# dismis = ['.git', '.idea', '.gitignore', 'readme.md']
+# put = 'C:/Users/Топлог/PycharmProjects/SkillBox/Projekt/'
+# out = search(put)
+# print(out)
+# for i in out:
+#     data = open(i, 'r', encoding='UTF-8').read()
+#     script = open('script.txt', 'a', encoding='UTF-8')
+#     script.write('\n')
+#     script.write(data)
+#     script.write('\n')
+#     script.write('*' * 40)
+#     script.write('\n')
+#     script.close()
 
-        if i_elem.endswith('.py'):
-            list_.append(path)
-        if os.path.isdir(path):
-            result = search(path)
-    return list_
+# 23 урок Ошибки и исключения
 
-dismis = ['.git', '.idea', '.gitignore', 'readme.md']
-put = 'C:/Users/Топлог/PycharmProjects/SkillBox/Projekt/'
-out = search(put)
-print(out)
-for i in out:
-    data = open(i, 'r', encoding='UTF-8').read()
-    script = open('script.txt', 'a', encoding='UTF-8')
-    script.write('\n')
-    script.write(data)
-    script.write('\n')
-    script.write('*' * 40)
-    script.write('\n')
-    script.close()
+BRUCE_WILLIS = 42
+
+input_data = input('Введите строку: ')
+try:
+    leeloo = int(input_data[4])
+except ValueError:
+    print('невозможно преобразовать к числу')
+except IndexError:
+    print('выход за границы списка')
+
+try:
+    result = BRUCE_WILLIS * leeloo
+    print(f'- Leeloo Dallas! Multi-pass № {result}!')
+except:
+    print('остальные исключения')
