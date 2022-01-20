c3711e303c25a7f6620c025d9d0a0091c6ca03ce
Ilya <kanaplyasm@gmail.com>
2022-01-20 10:16:10 +0300

    Done task25-5

--- Module25/Task25-5/Task-25-5.py
+++ Module25/Task25-5/Task-25-5.py
@@ -35,6 +35,9 @@ class Vehicle:
     def get_position(self):
         return self.__position
 
+    def get_direction(self):
+        return self.__direction
+
     def move(self):
         move_range = 10
         direction = self.direction_dict[self.__direction]
@@ -49,10 +52,40 @@ class Vehicle:
     def __str__(self):
         return 'Координаты равны {}'.format(self.get_position())
 
+
+class Bus(Vehicle):
+    def __init__(self, x, y, direction, people, money = 0):
+        super().__init__(x, y, direction)
+        self.__people = people
+        self.__money = money
+
+    def in_bus(self, number):
+        self.__people += number
+
+    def out_bus(self, number):
+        self.__people -= number
+
+    def move(self):
+        move_range = 10
+        cost = 5
+        direction = self.direction_dict[self.get_direction()]
+        direction /= 57.2958
+        y = move_range * math.sin(direction)
+        x = move_range * math.cos(direction)
+        self.set_position(x, y)
+        self.__money += move_range * self.__people * cost
+
+    def __str__(self):
+        return '{}, {}, {}'.format(self.get_position(), self.__people, self.__money)
+
+
 car = Vehicle(0, 0, 2)
+bus = Bus(0, 0, 3, 5)
 print(car)
 car.move()
 print(car)
 car.change_direction(11)
 car.move()
 print(car)
+bus.move()
+print(bus)
