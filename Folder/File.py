8150adba28130822bc296abafa90fb733d165cc0
Ilya <kanaplyasm@gmail.com>
2022-03-23 02:18:17 +0300

    done

--- Folder/File.py
+++ Folder/File.py
@@ -7,3 +7,5 @@ bot = telebot.TeleBot('5246923628:AAGR10Nt2gFZ8vQoqz6I4TpL7cAiPVaNQfg')
 @bot.message_handler(commands=['start'])
 def welcome(message):
     bot.send_message(message.chat.id, 'Привет, {name}!'.format(name=message.from_user))
+
+bot.polling(none_stop=True)
