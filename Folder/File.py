fb205198d2be4504f5fe3588b10f06a58ad485e6
Ilya <kanaplyasm@gmail.com>
2022-03-23 02:13:49 +0300

    done 31-3

---
+++ Folder/File.py
@@ -0,0 +1,9 @@
+import telebot
+import requests
+
+
+bot = telebot.TeleBot('5246923628:AAGR10Nt2gFZ8vQoqz6I4TpL7cAiPVaNQfg')
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id, 'Привет, {name}!'.format(name=message.from_user))
