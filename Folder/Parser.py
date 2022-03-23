4010d73f589ebdb5bfd4e14159776ca730b23e86
Ilya <kanaplyasm@gmail.com>
2022-03-23 10:46:09 +0300

    Started Lowprice

---
+++ Folder/Parser.py
@@ -0,0 +1,17 @@
+import json
+import re
+
+import requests
+
+# data: dict
+def parsing():
+	url = "https://hotels4.p.rapidapi.com/locations/v2/search"
+	querystring = {"query":"new york","locale":"en_US","currency":"USD"}
+	headers = {
+		"X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+		"X-RapidAPI-Key": "7fa207022cmsh412a4ad33fcf0b9p16a1a3jsn22b953c844ee"
+	}
+	response = requests.request("GET", url, headers=headers, params=querystring)
+	print(response.text)
+
+parsing()
