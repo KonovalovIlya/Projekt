d712c7544d27289fc73b6b06b3849706dd331e8f
Ilya <kanaplyasm@gmail.com>
2022-04-12 18:26:27 +0300

    inprogress Telebot command /lowprice

--- Folder/File.py
+++ Folder/File.py
@@ -4,115 +4,113 @@ from Parser import parsing
 import requests
 
 
-# bot = telebot.TeleBot('5246923628:AAGR1ONt2gFZ8vQoqz6I4TpL7cAiPVaNQfg')
+bot = telebot.TeleBot('5246923628:AAGR1ONt2gFZ8vQoqz6I4TpL7cAiPVaNQfg')
+info = dict()
 
-#
-# @bot.message_handler(commands=['start'])
-# def welcome(message):
-#     bot.send_message(message.chat.id, 'Привет, {name}!'.format(name=message.from_user.first_name))
-#
-#
-# @bot.message_handler(commands=['lowprice'])
-# def lowprice(message):
-#     bot.send_message(message.chat.id, 'Давайте начнем')
-#     get_message(message)
-#
-# @bot.message_handler(content_types=['text'])
-# def get_message(message):
-#     # Город, где будет проводиться поиск.
-#     bot.send_message(message.chat.id, 'В каком городе подобрать отель?')
-#     bot.register_next_step_handler(message, get_city)
-#
-# def get_city(message):
-#     info['city'] = message.text
-#     bot.send_message(message.chat.id, 'Укажите кол-во отелей (не больше пяти)')
-#     bot.register_next_step_handler(message, get_amount)
-#
-# def get_amount(message):
-#     global info
-#     # 2. Количество отелей, которые необходимо вывести в результате(не больше заранее определённого максимума).
-#     # bot.send_message(message.chat.id, 'Укажите кол-во отелей (не больше пяти)')
-#     # while int(message.text) > 5:
-#     #     bot.send_message(message.chat.id, 'Кол-во отелей должно быть не больше пяти)')
-#     info['amount'] = message.text
-#     bot.send_message(message.chat.id, 'Укажите даты въезда и выезда через запятую в формате гггг-мм-дд')
-#     bot.register_next_step_handler(message, get_date)
-#
-#
-# def get_date(message):
-#     global info
-#     info['check_in'] = message.text.split(', ')[0]
-#     info['check_out'] = message.text.split(', ')[1]
-#     bot.send_message(message.chat.id, 'Загрузить фото по отелям?')
-#     bot.register_next_step_handler(message, get_foto)
-#
-# def get_foto(message):
-#     if message.text == 'Да':
-#     # 3. Необходимость загрузки и вывода фотографий для каждого отеля(“Да / Нет”)
-#     # bot.send_message(message.chat.id, 'Загрузить фото по отелям?')
-#         bot.send_message(message.chat.id, 'Сколько фото(не больше пяти)?')
-#         bot.register_next_step_handler(message, get_foto_amount)
-#     else:
-#         bot.register_next_step_handler(message, get_foto_amount)
-#
-#
-# def get_foto_amount(message):
-#     global info
-#     # if message.text == 'Да':
-#         # bot.send_message(message.chat.id, 'Сколько фото(не больше пяти)?')
-#     info['foto_amount'] = int(message.text)
-#         # a.При положительном ответе пользователь также вводит количество необходимых фотографий(не больше заранее
-#         #         определённого максимума)
-#     keyboard = types.InlineKeyboardMarkup()
-#     key_yes = types.InlineKeyboardButton(text='Да', callback_data='Yes')
-#     keyboard.add(key_yes)
-#     key_no = types.InlineKeyboardButton(text='Нет', callback_data='No')
-#     keyboard.add(key_no)
-#     info_all = 'Ищем {amount} отелей в городе {city} с {foto_amount} фото'.format(
-#         amount=info['amount'],
-#         city=info['city'],
-#         foto_amount=info['foto_amount']
-#     )
-#     bot.send_message(message.from_user.id, text=info_all, reply_markup=keyboard)
-#
-#
-# @bot.callback_query_handler(func=lambda call: True)
-# def callback_worker(call):
-#     global info
-#     if call.data == "Yes": #call.data это callback_data, которую мы указали при объявлении кнопки
-#         res = parsing(info)#код сохранения данных, или их обработки
-#         for i in range(3):
-#             bot.send_message(
-#                 call.message.chat.id, 'Отель - {name}, адрес - {street}, расстояние от центра - {distance}, '
-#                                       'цена за ночь - {price}, стоимость за указанные даты - {price_for_all}'.format(
-#                     name=res[i][0],
-#                     street=res[i][1],
-#                     distance=res[i][2],
-#                     price=res[i][3],
-#                     price_for_all=res[i][4]
-#                 ))
-#     elif call.data == "No":
-#          pass #переспрашиваем
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id, 'Привет, {name}!'.format(name=message.from_user.first_name))
 
-if __name__ == '__main__':
-    info = dict()
-    info['city'] = 'New York'
-    info['amount'] = '4'
-    info['check_in'] = '2022-03-28'
-    info['check_out'] = '2022-03-31'
-    info['foto_amount'] = '2'
-    res = parsing(info)
-    for i in range(int(info['amount'])):
-        print(
-            'Отель - {name}, адрес - {street}, расстояние от центра - {distance}, '
-            'цена за ночь - {price}, стоимость за указанные даты - {price_for_all}'.format(
+
+@bot.message_handler(commands=['lowprice'])
+def lowprice(message):
+    bot.send_message(message.chat.id, 'Давайте начнем')
+    get_message(message)
+
+@bot.message_handler(content_types=['text'])
+def get_message(message):
+    # Город, где будет проводиться поиск.
+    bot.send_message(message.chat.id, 'В каком городе подобрать отель?')
+    bot.register_next_step_handler(message, get_city)
+
+def get_city(message):
+    info['city'] = message.text
+    bot.send_message(message.chat.id, 'Укажите кол-во отелей (не больше пяти)')
+    bot.register_next_step_handler(message, get_amount)
+
+def get_amount(message):
+    # 2. Количество отелей, которые необходимо вывести в результате(не больше заранее определённого максимума).
+    # bot.send_message(message.chat.id, 'Укажите кол-во отелей (не больше пяти)')
+    # while int(message.text) > 5:
+    #     bot.send_message(message.chat.id, 'Кол-во отелей должно быть не больше пяти)')
+    info['amount'] = message.text
+    bot.send_message(message.chat.id, 'Укажите даты въезда и выезда через запятую в формате гггг-мм-дд')
+    bot.register_next_step_handler(message, get_date)
+
+
+def get_date(message):
+    info['check_in'] = message.text.split(', ')[0]
+    info['check_out'] = message.text.split(', ')[1]
+    bot.send_message(message.chat.id, 'Загрузить фото по отелям?')
+    bot.register_next_step_handler(message, get_foto)
+
+def get_foto(message):
+    if message.text == 'Да':
+    # 3. Необходимость загрузки и вывода фотографий для каждого отеля(“Да / Нет”)
+    # bot.send_message(message.chat.id, 'Загрузить фото по отелям?')
+        bot.send_message(message.chat.id, 'Сколько фото(не больше пяти)?')
+        bot.register_next_step_handler(message, get_photo_amount)
+    else:
+        bot.register_next_step_handler(message, get_photo_amount)
+
+
+def get_photo_amount(message):
+    # if message.text == 'Да':
+        # bot.send_message(message.chat.id, 'Сколько фото(не больше пяти)?')
+    info['foto_amount'] = int(message.text)
+        # a.При положительном ответе пользователь также вводит количество необходимых фотографий(не больше заранее
+        #         определённого максимума)
+    keyboard = types.InlineKeyboardMarkup()
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='Yes')
+    keyboard.add(key_yes)
+    key_no = types.InlineKeyboardButton(text='Нет', callback_data='No')
+    keyboard.add(key_no)
+    info_all = 'Ищем {amount} отелей в городе {city} с {foto_amount} фото'.format(
+        amount=info['amount'],
+        city=info['city'],
+        foto_amount=info['foto_amount']
+    )
+    bot.send_message(message.from_user.id, text=info_all, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "Yes": #call.data это callback_data, которую мы указали при объявлении кнопки
+        res = parsing(info)#код сохранения данных, или их обработки
+        for i in range(int(info.get('amount'))):
+            bot.send_message(
+                call.message.chat.id, 'Отель - {name}, адрес - {street}, расстояние от центра - {distance}, '
+                                      'цена за ночь - {price}, стоимость за указанные даты - {price_for_all}, {photos}'.format(
                     name=res[i][0],
                     street=res[i][1],
                     distance=res[i][2],
                     price=res[i][3],
-                    price_for_all=res[i][4]
-                )
-        )
+                    price_for_all=res[i][4],
+                    photos=res[i][5:]
+                ))
+    elif call.data == "No":
+         pass #переспрашиваем
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
+    # info = dict()
+    # info['city'] = 'New York'
+    # info['amount'] = '4'
+    # info['check_in'] = '2022-03-28'
+    # info['check_out'] = '2022-03-31'
+    # info['foto_amount'] = '2'
+    # res = parsing(info)
+    # for i in range(int(info['amount'])):
+    #     print(
+    #         'Отель - {name}, адрес - {street}, расстояние от центра - {distance}, '
+    #         'цена за ночь - {price}, стоимость за указанные даты - {price_for_all}'.format(
+    #                 name=res[i][0],
+    #                 street=res[i][1],
+    #                 distance=res[i][2],
+    #                 price=res[i][3],
+    #                 price_for_all=res[i][4]
+    #             )
+    #     )
 #             bot.send_message(
 #                 call.message.chat.id, 'Отель - {name}, адрес - {street}, расстояние от центра - {distance}, '
 #                                       'цена за ночь - {price}, стоимость за указанные даты - {price_for_all}'.format(
@@ -122,4 +120,4 @@ if __name__ == '__main__':
 #                     price=res[i][3],
 #                     price_for_all=res[i][4]
 #                 ))
-    # bot.polling(none_stop=True)
+
--- Folder/Parser.py
+++ Folder/Parser.py
@@ -7,7 +7,9 @@ from typing import Dict, List
 # data: dict
 def parsing(dict_: Dict = None) -> List:
 	url_1 = "https://hotels4.p.rapidapi.com/locations/v2/search"
-	querystring_1 = {"query":"new york","locale":"en_US","currency":"USD"}
+	querystring_1 = {"query":dict_.get('city'),}
+	print(querystring_1)
+	# "locale":"en_US","currency":"USD"}
 	headers_1 = {
 		"X-RapidAPI-Host": "hotels4.p.rapidapi.com",
 		"X-RapidAPI-Key": "7fa207022cmsh412a4ad33fcf0b9p16a1a3jsn22b953c844ee"
@@ -21,6 +23,7 @@ def parsing(dict_: Dict = None) -> List:
 		"destinationId": result_1[1], "pageNumber": "1", "pageSize": "25", "checkIn": dict_['check_in'],
 		"checkOut": dict_['check_out'], "adults1": "1", "sortOrder": "PRICE", "locale": "en_US", "currency": "USD"
 	}
+	print(querystring_2)
 	headers_2 = {
 		"X-RapidAPI-Host": "hotels4.p.rapidapi.com",
 		"X-RapidAPI-Key": "7fa207022cmsh412a4ad33fcf0b9p16a1a3jsn22b953c844ee"
@@ -31,16 +34,22 @@ def parsing(dict_: Dict = None) -> List:
 	result_2 = result_2[:int(dict_.get('amount'))]
 
 	url_3 = "https://hotels4.p.rapidapi.com/properties/get-hotel-photos"
-	querystring_3 = {"id": "1178275040"}
-	headers_3 = {
-		"X-RapidAPI-Host": "hotels4.p.rapidapi.com",
-		"X-RapidAPI-Key": "7fa207022cmsh412a4ad33fcf0b9p16a1a3jsn22b953c844ee"
-	}
-	response_3 = requests.request("GET", url_3, headers=headers_3, params=querystring_3)
-	# print(response_3.text)
-	data_3 = json.loads(response_3.text)
-	with open('Photos.json', 'w') as photos:
-		json.dump(response_3.text, photos, indent=4)
+	list_photos = []
+	for i in result_2:
+		querystring_3 = {"id": i.get('id')}
+		print(querystring_3)
+		headers_3 = {
+			"X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+			"X-RapidAPI-Key": "7fa207022cmsh412a4ad33fcf0b9p16a1a3jsn22b953c844ee"
+		}
+		response_3 = requests.request("GET", url_3, headers=headers_3, params=querystring_3)
+		# print(response_3.text)
+		data_3 = json.loads(response_3.text)
+		with open('Photos.json', 'w') as photos:
+			json.dump(response_3.text, photos, indent=4)
+		result_3 = recursion_3(data_3)
+		result_3 = result_3[:dict_.get('foto_amount')]
+		list_photos.append(result_3)
 
 	# print(result_2)
 	list_2 = [list() for _ in range(int(dict_.get('amount')))]
@@ -49,9 +58,10 @@ def parsing(dict_: Dict = None) -> List:
 		list_2[result_2.index(i)].append(i.get("address").get("streetAddress"))
 		list_2[result_2.index(i)].append(i.get("landmarks")[0].get("distance"))
 		list_2[result_2.index(i)].append(i.get("ratePlan").get("price").get("current"))
-		list_2[result_2.index(i)].append(
-			re.search(r'\$\d{3}', i.get().get("price").get("fullyBundledPricePerStay")).group()
-		)
+		ppd = re.search(r'\$\w?\W?\d{3}', i.get("ratePlan").get("price").get("fullyBundledPricePerStay")).group()
+		print(ppd)
+		list_2[result_2.index(i)].append(ppd)
+		list_2[result_2.index(i)].extend(list_photos[result_2.index(i)])
 	# print(list_2)
 	return list_2
 	# return recursion(data_1)
@@ -77,6 +87,13 @@ def recursion_2(data_: Dict) -> List:
 				return result
 		if i == 'results':
 			return data_.get(i)
+
+
+def recursion_3(data_: Dict) -> List:
+	list_photos = []
+	for i in data_.get('hotelImages'):
+		list_photos.append(i.get('baseUrl'))
+	return list_photos
 
 
 if __name__ == '__main__':
