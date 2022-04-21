544bbfdc76a2eb45be81b7231a5e5b42a882604c
Ilya <kanaplyasm@gmail.com>
2022-04-21 18:02:41 +0300

    Продолжаем домашку по джанго, осталось сделать комментарии.

--- Django/news_app/news/admin.py
+++ Django/news_app/news/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from news.models import News
+from news.models import News, Comment
 # Register your models here.
 
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     pass
--- Django/news_app/news/views.py
+++ Django/news_app/news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from news.models import News
 from django.views import generic, View
+from django.http import HttpResponseRedirect
+from news.models import News, Comment
+from news.forms import NewsForm, CommentForm
 
 
 # Create your views here.
@@ -11,10 +13,17 @@ class NewsListView(generic.ListView):
     queryset = News.objects.all()[:10]
 
 
+class CommentListView(generic.ListView):
+    model = Comment
+    template_name = 'news_detail.html'
+    context_object_name = 'comment_list'
+    queryset = Comment.objects.all()[:10]
+
+
 class NewsDetailView(generic.DetailView):
     model = News
-    template_name = 'news_detail.html'
-    context_object_name = 'news_detail'
+    # template_name = 'news_detail.html'
+    # context_object_name = 'news_detail'
 
 
 class NewsFormView(View):
@@ -33,5 +42,42 @@ class NewsFormView(View):
 
 
 class NewsFormEditView(View):
-    def get(self, request, profile_id):
-        news = News.objects.get(id=profile_id)
+    def get(self, request, news_id):
+        news = News.objects.get(id=news_id)
+        news_form = NewsForm(instance=news)
+        return render(request, 'news/news_form_redactor.html', context={'news_form': news_form, 'news_id': news_id})
+
+    def post(self, request, news_id):
+        news = News.objects.get(id=news_id)
+        news_form = NewsForm(request.POST, instance=news)
+
+        if news_form.is_valid():
+            news.save()
+        return render(request, 'news/news_form_redactor.html', context={'news_form': news_form, 'news_id': news_id})
+
+
+class CommentFormView(View):
+    # def get(self, request, news_id):
+    #     news = News.objects.get(id=news_id)
+    #     news_form = NewsForm(instance=news)
+    #     return render(request, 'news/news_form_redactor.html', context={'news_form': news_form, 'news_id': news_id})
+
+    # def post(self, request, comment):
+    #     comment = Comment.objects.get(id=comment_id)
+    #     comment_form = CommentForm(request.POST, instance=comment)
+    #
+    #     if comment_form.is_valid():
+    #         comment.save()
+    #     return render(request, 'news/news_detail.html', context={'comment_form': comment_form, 'comment_id': comment_id})
+
+    def get(self, request):
+        comment_form = CommentForm()
+        return render(request, 'news/news_detail.html', {'comment_form': comment_form})
+
+    def post(self, request):
+        comment_form = CommentForm(request.POST)
+
+        if comment_form.is_valid():
+            Comment.objects.create(**comment_form.cleaned_data)
+            return HttpResponseRedirect('/')
+        return render(request, 'news/news_detail.html', {'comment_form': comment_form})
