78d20d5639cc53d6b9f7ca4b8d2c837e17ee4045
Ilya <kanaplyasm@gmail.com>
2022-04-21 18:02:41 +0300

    Продолжаем домашку по джанго, осталось сделать комментарии.

--- Django/news_app/news/views.py
+++ Django/news_app/news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from news.models import News
 from django.views import generic, View
+from django.http import HttpResponseRedirect
+from news.models import News
+from news.forms import NewsForm
 
 
 # Create your views here.
@@ -13,8 +15,8 @@ class NewsListView(generic.ListView):
 
 class NewsDetailView(generic.DetailView):
     model = News
-    template_name = 'news_detail.html'
-    context_object_name = 'news_detail'
+    # template_name = 'news_detail.html'
+    # context_object_name = 'news_detail'
 
 
 class NewsFormView(View):
@@ -33,5 +35,15 @@ class NewsFormView(View):
 
 
 class NewsFormEditView(View):
-    def get(self, request, profile_id):
-        news = News.objects.get(id=profile_id)
+    def get(self, request, news_id):
+        news = News.objects.get(id=news_id)
+        news_form = NewsForm(instance=news)
+        return render(request, 'news/news_form_redactor.html', context={'news_form': news_form, 'news_id': news_id})
+
+    def post(self, request, news_id):
+        news = News.objects.get(id=news_id)
+        news_form = NewsForm(request.POST, instance=news)
+
+        if news_form.is_valid():
+            news.save()
+        return render(request, 'news/news_form_redactor.html', context={'news_form': news_form, 'news_id': news_id})
