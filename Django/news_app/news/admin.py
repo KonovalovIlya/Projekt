be0024b5e2e01eb590215ea42cd67bff9e6d0184
Ilya <kanaplyasm@gmail.com>
2022-04-22 18:02:35 +0300

    Домашка №6 сделана

--- Django/news_app/news/admin.py
+++ Django/news_app/news/admin.py
@@ -2,12 +2,38 @@ from django.contrib import admin
 from news.models import News, Comment
 # Register your models here.
 
+class CommentInLine(admin.StackedInline):
+    model = Comment
 
-@admin.register(News)
+
 class NewsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'created_at', 'updated_at', 'interest']
+    list_filter = ['interest']
+    inlines = [CommentInLine]
+
+    actions = ['mark_as_true', 'mark_as_false']
+
+    def mark_as_true(self, request, queryset):
+        queryset.update(interest=True)
+
+    def mark_as_false(self, request, queryset):
+        queryset.update(interest=False)
+
+    mark_as_true.short_description = 'Перевести в статус Активна'
+    mark_as_false.short_description = 'Перевести в статус Не активна'
 
 
-@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['user_name', 'comment', 'news']
+    list_filter = ['user_name']
+
+    actions = ['delete',]
+
+    def delete(self, request, queryset):
+        queryset.update(comment='Удалено администратором')
+
+    delete.short_description = 'Удалить содержание комментария'
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Comment, CommentAdmin)
